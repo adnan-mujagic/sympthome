902e73ae629e790b6fb10b7a1c7a0308ff013697
adnan-mujagic <adnanmujagic@outlook.com>
2021-03-14 19:12:14 +0100

    Adding Diseases DAO

---
+++ api/dao/DiseasesDao.class.php
@@ -0,0 +1,28 @@
+<?php
+require_once dirname(__FILE__)."/BaseDao.class.php";
+
+class DiseasesDao extends BaseDao{
+
+  public function __construct(){
+    parent::__construct();
+
+  }
+
+  public function get_diseases_by_id($id){
+    return $this->query_single("SELECT * FROM diseases WHERE id=:id",array("id"=>$id));
+  }
+
+  public function get_diseases_by_name($name){
+    return $this->query("SELECT * FROM diseases WHERE name LIKE '%$name%'",array());
+  }
+
+  public function update_disease($updates, $id){
+    $this->update("diseases",$updates,$id);
+  }
+
+  public function insert_disease($disease){
+    $this->insert("diseases",$disease);
+  }
+}
+
+ ?>
