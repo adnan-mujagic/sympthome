dbedcd1ea17a6df4f9fb3c52fc8f8e2c708e6053
adnan-mujagic <adnanmujagic@outlook.com>
2021-03-17 00:01:01 +0100

    Trying Flight

--- api/index.php
+++ api/index.php
@@ -4,17 +4,18 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once dirname(__FILE__)."/dao/BaseDao.class.php";
-require_once dirname(__FILE__)."/../vendor/autoload.php";
+require dirname(__FILE__)."/../vendor/autoload.php";
+require_once dirname(__FILE__)."/dao/UsersDao.class.php";
 
-
-Flight::register("usersDao", "UsersDao");
-
-
-
-
-Flight::route('GET /users', function(){
-    Flight::json(Flight::accountDao()->get_all(0,10));
+Flight::route("/",function(){
+  echo "Hello world";
 });
+
+Flight::route("/hello2",function(){
+  echo "Hello world TWO";
+});
+
+Flight::start();
 
 
  ?>
