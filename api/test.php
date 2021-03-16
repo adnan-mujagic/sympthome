fd3cb3720aa1574b30280f9f14fd49a2f036df3c
adnan-mujagic <adnanmujagic@outlook.com>
2021-03-17 00:00:39 +0100

    Testing out all the DAO classes

--- api/test.php
+++ api/test.php
@@ -1,26 +1,37 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
 
-require_once dirname(__FILE__). "/dao/UsersDao.class.php";
-require_once dirname(__FILE__)."/dao/SymptomsDao.class.php";
-require_once dirname(__FILE__)."/dao/MedicinesDao.class.php";
-require_once dirname(__FILE__)."/dao/DiseasesDao.class.php";
+  require_once dirname(__FILE__). "/dao/UsersDao.class.php";
+  require_once dirname(__FILE__)."/dao/SymptomsDao.class.php";
+  require_once dirname(__FILE__)."/dao/MedicinesDao.class.php";
+  require_once dirname(__FILE__)."/dao/DiseasesDao.class.php";
+  require_once dirname(__FILE__)."/dao/BodyPartsDao.class.php";
 
-$userdao = new UsersDao();
-$symdao = new SymptomsDao();
-$medicinedao = new MedicinesDao();
-$diseasedao = new DiseasesDao();
+  $userdao = new UsersDao();
+  $symdao = new SymptomsDao();
+  $medicinedao = new MedicinesDao();
+  $diseasedao = new DiseasesDao();
+  $bpdao = new BodyPartsDao();
 
 
-$updates=[
+  $bp = array(
+    "name"=>"Stomach"
+  );
 
-  "age"=>23,
-  "password"=>"not a strong password",
+  //TESTING HERE
+  //Basic functions of the following classes are working:
+  //-UsersDao
+  //-SymptomsDao
+  //-MedicinesDao
+  //-DiseasesDao
+  //-BodyPartsDao
 
-];
 
-print_r($userdao->update($updates,7));
 
- ?>
+  print_r($bpdao->get_symptoms_related_to_bodypart(2));
+
+
+
+?>
