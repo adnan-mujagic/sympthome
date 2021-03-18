d1eb3dd9ab3bef5361cfa048849f92fa283fcac2
adnan-mujagic <adnanmujagic@outlook.com>
2021-03-19 00:38:05 +0100

    Adding register and confirm functions

--- api/services/UserService.class.php
+++ api/services/UserService.class.php
@@ -1,10 +1,19 @@
 <?php
   require_once dirname(__FILE__)."/../dao/UsersDao.class.php";
   require_once dirname(__FILE__)."/BaseService.class.php";
+  require_once dirname(__FILE__)."/../config.php";
 
   class UserService extends BaseService{
     public function __construct(){
       parent::__construct(new UsersDao());
+    }
+
+    public function register($data){
+      $data["created_at"]=date(Config::DATE_FORMAT);
+      $data["token"]=md5(random_bytes(16));
+
+      // TODO: SEND EMAIL
+      return $this->add($data);
     }
 
     public function get_users($search,$offset,$limit){
@@ -35,6 +44,17 @@
                 GROUP BY d.id ";
       return $this->query($query, ["id"=>$user_id]);
     }
+
+    public function confirm($token){
+      $user = $this->dao->get_user_by_token($token);
+      if($user){
+        $this->update(["status"=>"ACTIVE"],$user["id"]);
+        return $this->get_by_id($user["id"]);
+
+      }
+    }
+
+
 
 
   }
