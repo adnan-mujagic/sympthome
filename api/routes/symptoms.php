87ae40206d7aaad15e9a1e3d09cf3a284a2214e4
adnan-mujagic <adnanmujagic@outlook.com>
2021-03-19 02:56:57 +0100

    Some symptoms routes

--- api/routes/symptoms.php
+++ api/routes/symptoms.php
@@ -1,5 +1,28 @@
 <?php
+Flight::route("GET /symptoms", function(){
+  $search = Flight::query("search");
+  if($search){
+    Flight::json(Flight::symptomService()->get_symptoms_by_name($search));
+  }
+  else{
+    Flight::json(Flight::symptomService()->get_all());
+  }
 
+});
+
+Flight::route("GET /symptoms/@id", function($id){
+  Flight::json(Flight::symptomService()->get_by_id($id));
+});
+
+Flight::route("POST /symptoms", function(){
+  $data = Flight::request()->data->getData();
+  Flight::json(Flight::symptomService()->add($data));
+});
+
+Flight::route("PUT /symptoms/@id",function($id){
+  $data = Flight::request()->data->getData();
+  Flight::json(Flight::symptomService()->update($data,$id));
+});
 
 
  ?>
