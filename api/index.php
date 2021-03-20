ce937215002af676ff028e2241dad4f7f787abd2
adnan-mujagic <adnanmujagic@outlook.com>
2021-03-20 20:59:26 +0100

    Adding required files to index.php

--- api/index.php
+++ api/index.php
@@ -9,6 +9,7 @@ error_reporting(E_ALL);
 require_once dirname(__FILE__)."/../vendor/autoload.php";
 require_once dirname(__FILE__)."/services/UserService.class.php";
 require_once dirname(__FILE__)."/services/SymptomService.class.php";
+require_once dirname(__FILE__)."/services/DiseaseService.class.php";
 
 Flight::set('flight.log_errors', TRUE);
 
@@ -16,11 +17,12 @@ Flight::set('flight.log_errors', TRUE);
 Flight::register("user","UsersDao");
 Flight::register("userService","UserService");
 Flight::register("symptomService", "SymptomService");
+Flight::register("diseaseService", "DiseaseService");
 
-/*Error exception function*/
+/*Error exception function
 Flight::map("error",function(Exception $e){
   Flight::json(["message"=>$e->getMessage()],$e->getCode());
-});
+});*/
 
 /*Utility function to return query parameters*/
 Flight::map("query",function($name,$default_value=NULL){
@@ -33,6 +35,7 @@ Flight::map("query",function($name,$default_value=NULL){
 //Here we need to add the routes that are placed in the other folder...
 require_once dirname(__FILE__)."/routes/users.php";
 require_once dirname(__FILE__)."/routes/symptoms.php";
+require_once dirname(__FILE__)."/routes/diseases.php";
 
 
 
