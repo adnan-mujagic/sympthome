f3e058261b0cd90a79baf2f20d4af6febbe08846
adnan-mujagic <adnanmujagic@outlook.com>
2021-03-20 21:04:16 +0100

    Support for offset, limit, and order

--- api/services/BaseService.class.php
+++ api/services/BaseService.class.php
@@ -14,8 +14,8 @@
       return $this->dao->get_by_id($id);
     }
 
-    public function get_all(){
-      return $this->dao->get_all();
+    public function get_all($offset = 0, $limit = 25, $order="-id"){
+      return $this->dao->get_all($offset, $limit, $order);
     }
 
     public function update($updates, $id){
