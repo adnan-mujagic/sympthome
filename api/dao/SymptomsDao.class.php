9d4695079aa3f30f71ffdd805783817e31b3109f
adnan-mujagic <adnanmujagic@outlook.com>
2021-03-21 13:02:35 +0100

    get_symptoms_by_name function deleted because it was no longer necessary

--- api/dao/SymptomsDao.class.php
+++ api/dao/SymptomsDao.class.php
@@ -10,15 +10,6 @@
       parent::__construct("symptoms");
     }
 
-    public function get_symptoms_by_name($name,$offset,$limit,$order){
-      list($column, $direction) = parent::parse_order($order);
-      return $this->query("SELECT *
-                           FROM symptoms
-                           WHERE LOWER(name) LIKE LOWER(CONCAT('%',:name,'%'))
-                           ORDER BY ".$column." ".$direction."
-                           LIMIT ".$limit." OFFSET ".$offset, array("name"=>$name));
-
-    }
   }
 
 
