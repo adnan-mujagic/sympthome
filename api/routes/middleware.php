3eaaa98c3c1a869ac603dcb578f592f8fff85ab3
adnan-mujagic <adnanmujagic@outlook.com>
2021-04-03 15:53:05 +0200

    Interceptors for admin and user routes

--- api/routes/middleware.php
+++ api/routes/middleware.php
@@ -1,23 +1,36 @@
 <?php
-  Flight::route("*",function(){
-    if(str_starts_with(Flight::request()->url,"/users/")){
-      if(str_starts_with(Flight::request()->url,"/users/byId")){
-        $headers = getallheaders();
-        $token = @$headers["Authentication"];
-        try {
-          $decoded = (array)\Firebase\JWT\JWT::decode($token, "SECRET", array('HS256'));
-          Flight::set('user', $decoded);
-          return TRUE;
 
-        } catch (\Exception $e) {
-          Flight::json(["message"=>"Make sure to log in before doing this action!"]);
-        }
-      }
+  //NORMAL USER ROUTES!
+  Flight::route("/users/*",function(){
+    try {
+      $decoded = (array)\Firebase\JWT\JWT::decode(Flight::header("Authentication"), Config::JWT_SECRET, array('HS256'));
+      Flight::set('user', $decoded);
       return TRUE;
+
+    } catch (\Exception $e) {
+      Flight::json(["message"=>"Make sure to log in before doing this action!"]);
+      die;
+    }
+  });
+
+  //ADMIN ROUTES
+  Flight::route("/admin/*", function(){
+    try {
+      $decoded = (array) \Firebase\JWT\JWT::decode(Flight::header("Authentication"),Config::JWT_SECRET, array("HS256"));
+      if($decoded["role"]!="ADMIN"){
+        throw new Exception("This route requires admin credentials!",403);
+      }
+      Flight::set("user", $decoded);
+      return TRUE;
+    } catch (\Exception $e) {
+      Flight::json(["message"=>$e->getMessage()],401);
+      die;
     }
 
 
 
   })
+
+
 
  ?>
