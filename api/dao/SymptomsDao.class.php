b66a5b2c6fc06771baddc30abecff2d8656e061c
adnan-mujagic <adnanmujagic@outlook.com>
2021-05-21 14:28:46 +0200

    Small upgrade to the backend - it returns only the ACTIVE symptoms

--- api/dao/SymptomsDao.class.php
+++ api/dao/SymptomsDao.class.php
@@ -13,10 +13,10 @@
     public function get_user_symptoms($user_id,$offset,$limit,$order,$search=NULL){
       list($column, $direction) = $this->parse_order($order);
       $params = [];
-      $query = "SELECT s.name FROM symptoms s
+      $query = "SELECT s.id AS id,s.name AS name FROM symptoms s
                 JOIN user_symptom_log usl ON usl.symptom_id = s.id
                 JOIN users u ON u.id = usl.user_id
-                WHERE u.id =".$user_id;
+                WHERE u.id =".$user_id." AND usl.status = 'ACTIVE'";
       if(isset($search)){
         $query=$query." AND LOWER(s.name) LIKE LOWER(CONCAT('%',:search,'%'))";
         $params["search"]=$search;
