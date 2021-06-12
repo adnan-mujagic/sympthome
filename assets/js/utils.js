5ec0c85dcea0009ddea4e48b42053956aac3f6bb
adnan-mujagic <adnanmujagic@outlook.com>
2021-06-12 19:46:51 +0200

    Code Refactoring

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,18 +1,26 @@
-function jsonize(selector){
-  var data = $(selector).serializeArray();
-  var form_data={};
-  for(var i = 0; i<data.length;i++){
-    form_data[data[i].name] = data[i].value;
+class Utils{
+  static jsonize(selector){
+    var data = $(selector).serializeArray();
+    var form_data={};
+    for(var i = 0; i<data.length;i++){
+      form_data[data[i].name] = data[i].value;
+    }
+    console.log(form_data);
+    return form_data;
   }
-  console.log(form_data);
-  return form_data;
-}
 
-function parse_jwt (token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    return JSON.parse(jsonPayload);
-};
+  static parse_jwt (token) {
+      var base64Url = token.split('.')[1];
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+      return JSON.parse(jsonPayload);
+  }
+
+  static formalize(selector, data){
+    for (const attr in data){
+      $(selector+" *[name='"+attr+"']").val(data[attr]);
+    }
+  }
+}
