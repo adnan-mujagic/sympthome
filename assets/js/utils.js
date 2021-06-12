6c75c71ff7065f6d7d4361195943de15f3cc8d0a
adnan-mujagic <adnanmujagic@outlook.com>
2021-06-12 12:18:18 +0200

    Admin functionalities removed for regular users!

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -7,3 +7,12 @@ function jsonize(selector){
   console.log(form_data);
   return form_data;
 }
+
+function parse_jwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
+};
